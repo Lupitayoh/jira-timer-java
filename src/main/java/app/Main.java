93b035d209fe16c2d8f6eea2ed8fb661286a4931
Lupitayoh <lupitayoh-5@hotmail.com>
2018-12-12 23:31:28 -0600

    Staring with the design

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,16 +1,30 @@
 package app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    /**
+     * Holds a reference to the resource folder of all fxml files
+     */
+    public static String VIEWS = "/views/";
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(new Scene(new Pane(), 800, 600));
-        primaryStage.show();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(VIEWS + "login.fxml"));
+            Scene scene = new Scene(root,600,800);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String []args) {
