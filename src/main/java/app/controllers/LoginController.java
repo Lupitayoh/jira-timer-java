cb3599ca52721822632a86151eef42925d219dc8
Lupitayoh <lupitayoh-5@hotmail.com>
2018-12-29 23:29:56 -0600

    Add ids to textfields and get the info in controller

--- src/main/java/app/controllers/LoginController.java
+++ src/main/java/app/controllers/LoginController.java
@@ -1,4 +1,26 @@
 package app.controllers;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LoginController {
+
+    @FXML
+    private JFXTextField txtHost;
+
+    @FXML
+    private JFXTextField txtUsername;
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    public void login(ActionEvent event) {
+        System.out.println("Host: " + txtHost.getText());
+        System.out.println("Password: " + txtPassword.getText());
+        System.out.println("Username: " + txtUsername.getText());
+        
+    }
+
 }
--- src/main/java/app/models/User.java
+++ src/main/java/app/models/User.java
@@ -1,4 +1,40 @@
 package app.models;
 
 public class User {
+
+    private String host;
+    private String username;
+    private String password;
+
+    public User() {}
+
+    public User(String host, String username, String password) {
+        this.host = host;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getHost(){
+        return host;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setUsername(String username) {
+        this.password = password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
